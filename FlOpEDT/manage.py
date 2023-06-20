744468f4d5b7447529ca86a72e2bb5c9396b0293
etu <nikamelie09@gmail.com>
2023-06-20 10:16:42 +0200

    update manage.py to run both tests and the app

--- FlOpEDT/manage.py
+++ FlOpEDT/manage.py
@@ -35,11 +35,10 @@ def run_api_tests():
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FlOpEDT.settings.local")
     try:
+        from django.core.management import execute_from_command_line
         if len(sys.argv) > 1 and sys.argv[1] == 'test':
             run_api_tests()
-        else:
-            from django.core.management import execute_from_command_line
-            execute_from_command_line(sys.argv)
+        
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
@@ -53,4 +52,4 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    #execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
